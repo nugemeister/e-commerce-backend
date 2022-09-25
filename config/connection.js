bb824e32a5111a929c2b6577d1007d7a233bc2e2
Ali Nugen <alexandra.nugen@gmail.com>
2022-09-25 15:52:52 -0700

    updating connection info and config for route testing

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,10 @@
+// access to env variables
 require('dotenv').config();
 
+// import sequelize dependancy
 const Sequelize = require('sequelize');
 
+// use env var to connect to db
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
