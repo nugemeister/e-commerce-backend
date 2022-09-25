883ded86f774325cf2944c1496a3e45882c66f25
Ali Nugen <alexandra.nugen@gmail.com>
2022-09-25 15:46:07 -0700

    updating to fix missing product name and seed data

--- models/Product.js
+++ models/Product.js
@@ -17,6 +17,10 @@ Product.init(
       primaryKey: true,
       autoIncrement: true
     },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
     price: {
       type: DataTypes.DECIMAL,
       allowNull: false,
